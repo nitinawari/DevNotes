e0e222eb447c5c05ab4697be5ececcd02946a0c2
Nitin Awari <nitinawari.dev@gmail.com>
2025-08-08 00:00:48 +0530

    update baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,13 @@ const config: Config = {
   url: 'https://nitinawari.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/devnotes/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nitinawari', // Usually your GitHub org/user name.
   projectName: 'devnotes', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
